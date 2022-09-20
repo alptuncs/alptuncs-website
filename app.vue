50080c415783d90c1afeffebf90816f0b1b01778
alptunc.sever <alptuncsever@gmail.com>
2022-09-20 19:43:58 +0300

    testing script and html

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+
+    const counterState = reactive({
+        counter: 0,
+    });
+</script>
+
 <template>
-  <div>
-      <h1>Welcome</h1>
-  </div>
+    <div>
+        <h1>Welcome</h1>
+        <div>
+            <h3>You have clicked the button  {{counterState.counter}}  times</h3>
+        </div>
+        <div>
+            <button @click="counterState.counter += 1">
+                Increase
+            </button>
+            <button @click="counterState.counter -= 1">
+                Decrease
+            </button>
+            <button @click="counterState.counter = 0">
+                Reset
+            </button>
+        </div>
+    </div>
 </template>
