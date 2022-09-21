95e3e4432face4eb7fa478237cc4e9c788177c81
alptunc.sever <alptuncsever@gmail.com>
2022-09-21 11:39:11 +0300

    add styling

--- app.vue
+++ app.vue
@@ -6,21 +6,50 @@
 </script>
 
 <template>
-    <div>
-        <h1>Welcome</h1>
+    <div class="container">
+        <h1 class="website-title">Welcome</h1>
         <div>
-            <h3>You have clicked the button  {{counterState.counter}}  times</h3>
+            <h3 class="info">You have clicked the button  {{counterState.counter}}  times</h3>
         </div>
-        <div>
-            <button @click="counterState.counter += 1">
+        <div class="button-container">
+            <button class="interactive-button"
+                    @click="counterState.counter += 1">
                 Increase
             </button>
-            <button @click="counterState.counter -= 1">
+            <button class="interactive-button"
+                    @click="counterState.counter -= 1">
                 Decrease
             </button>
-            <button @click="counterState.counter = 0">
+            <button class="interactive-button"
+                    @click="counterState.counter = 0">
                 Reset
             </button>
         </div>
+        <h1><nuxt-link to:"/about">About</nuxt-link></h1>
     </div>
 </template>
+
+<style scoped>
+    .container {
+        text-align: center;
+    }
+
+    .website-title {
+        font-family: Arial, Helvetica, sans-serif;
+        color: rgb(50,50,256);
+        text-align: center;
+    }
+
+    .info {
+        font-family: Arial,Arial, Helvetica, sans-serif;
+        text-align: center;
+    }
+
+    .button-container {
+        position: relative;
+    }
+
+    .interactive-button {
+        text-align: center;
+    }
+</style>
