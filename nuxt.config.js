c2ec4b36cef7f2c4c204afca7a6ea692d6427efe
alptunc.sever <alptuncsever@gmail.com>
2022-09-22 15:46:56 +0300

    new project with jscript

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - alptuncs-website',
@@ -32,8 +35,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
   ],
@@ -63,6 +64,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  },
-  target: 'static'
+  }
 }
